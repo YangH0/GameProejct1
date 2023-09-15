5a59204bd468f0f881b746a2da393bb60f8a7b8c
YangH0 <didwogh05@gmail.com>
2023-09-15 16:52:10 +0900

    version change

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,42 +20,56 @@ public class PlayerMovement : MonoBehaviour
 
     private void Awake()
     {
-        
-    }
-
-    void Update()
-    {
-        //xMoveInput = Mathf.Lerp(xMoveInput, Input.GetAxis("Horizontal"), Time.deltaTime*10);
-        //zMoveInput = Mathf.Lerp(zMoveInput, Input.GetAxis("Vertical"), Time.deltaTime*10);
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+    }
 
-        xMoveInput = Input.GetAxis("Horizontal");
-        zMoveInput = Input.GetAxis("Vertical");
+    private void Update()
+    {
+        SetAim();
+        AutoAttack();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        SetRotation();
+    }
+
+    private void AutoAttack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, buleltStart.transform.position, Quaternion.LookRotation(shootTarget));
+        }
+    }
+
+    private void SetAim()
+    {
+        Debug.DrawRay(cam.transform.position, cam.transform.forward * 15, Color.red);
+        shootTarget = cam.transform.position + cam.transform.forward * 30;
+        shootTarget = (shootTarget - transform.position).normalized;
+        Debug.DrawRay(transform.position, shootTarget * 10, Color.red);
+    }
+
+    private void Move()
+    {
+        xMoveInput = Input.GetAxisRaw("Horizontal");
+        zMoveInput = Input.GetAxisRaw("Vertical");
 
         playerVelocity = new Vector3(xMoveInput, 0, zMoveInput);
         playerVelocity = cam.transform.TransformDirection(playerVelocity);
         playerVelocity.y = 0;
         playerVelocity = playerVelocity.normalized;
+        playerRigid.velocity = playerVelocity * moveSpeed;
+    }
 
+    private void SetRotation()
+    {
         playerRotation = cam.transform.forward;
         playerRotation.y = 0;
 
         transform.rotation = Quaternion.LookRotation(playerRotation);
-
-        playerRigid.velocity = playerVelocity * moveSpeed;
-
-
-        Debug.DrawRay(cam.transform.position, cam.transform.forward * 15, Color.red);
-        shootTarget = cam.transform.position + cam.transform.forward * 30;
-        shootTarget = (shootTarget - transform.position).normalized;
-        Debug.DrawRay(transform.position, shootTarget * 10, Color.red);
-
-
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Instantiate(bullet,buleltStart.transform.position,Quaternion.LookRotation(shootTarget));
-        }
     }
+
 }
